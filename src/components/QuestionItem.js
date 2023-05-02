26fd0b202d03276d3ba954daa8dc901036383033
joseph <josephmacharia575@gmail.com>
2023-05-02 15:17:08 +0300

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -8,6 +8,41 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+  async function handleDelete() {
+    try {
+      await fetch(`http://localhost:4000/questions/${question.id}`, {
+        method: "DELETE",
+      });
+      console.log("Deleted", question);
+      onDeleteQuestion(question.id);
+    } 
+  }
+  async function handleSelectChange(event) {
+    const newCorrectIndex = Number(event.target.value);
+    const updatedQuestion = {
+      ...question,
+      correctIndex: newCorrectIndex,
+    };
+    const updatedQuestions = questions.map((q) =>
+    q.id === question.id ? updatedQuestion : q
+  );
+  setQuestions(updatedQuestions);
+
+  try {
+    await fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: newCorrectIndex,
+      }),
+    });
+    console.log("Question updated", updatedQuestion);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
   return (
     <li>
@@ -15,9 +50,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleSelectChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>DELETE </button>
     </li>
   );
 }
